5b35b5c8ef30f43503566fd30f3efb8d89826e56
yihong <yihongliu00@gmail.com>
2019-07-12 17:56:33 -0400

    working rotational vision bound

--- Agent.cpp
+++ Agent.cpp
@@ -13,17 +13,26 @@ Agent::Agent(int x, int y, int r, t_agent type)
 	Agent::r = r;
 	Agent::type = type;
 
-	//std::cout<<"type "<<type<<std::endl;
 	if(type == t_agent::PRED)
 	{
-		v_vector[0] = 4;
-		v_vector[1] = M_PI/4;
+		vision_vector[0] = 2*r;
+		vision_vector[1] = M_PI/4;
 	}
 	else
 	{
-		v_vector[0] = 2;
-		v_vector[1] = M_PI;
+		vision_vector[0] = 2;
+		vision_vector[1] = M_PI;
 	}
+	//rotation_matrix[0] = 0;
+
+	//since the angle is measured with respect to y axis, the x displacement is actually rsin(theta), same goes for y displacement
+	vx_left = (vision_vector[0]+r)*sin(vision_vector[1]);
+	vx_right = (vision_vector[0]+r)*sin(vision_vector[1]);
+	vy_top = r+vision_vector[0]*cos(rotation);
+	std::cout<<vy_top<<std::endl;
+	vy_bottom = r;
+
+	
 	
 	Agent::rotation = 0;
 }
@@ -32,14 +41,9 @@ Agent::~Agent()
 {
 }
 
-double gene_func(double x)
+double gene_func(int input)
 {
-	return 1/(1+std::exp(-x));
-}
-
-double Agent::gene_function(double input, double weight, double bias)
-{
-	return 0;
+	return 1/(1+std::exp(-input));
 }
 
 Agent* Agent::reproduce()
@@ -50,11 +54,43 @@ Agent* Agent::reproduce()
 void Agent::render(sf::RenderWindow* window)
 {
 	window->draw(shape);
+	//for(int i = x-vx)
+	//std::cout<<"bounds: "<<(x+r-vx_left)<<" "<<x+r
+	sf::RectangleShape* temp;
+	for(int i = -vx_left; i<vx_right; i++)
+	{
+		for(int j = -vy_top; j<0; j++)
+		{
+			temp = new sf::RectangleShape(sf::Vector2f(1, 1));
+			temp->setPosition(sf::Vector2f((x+r)+(i*rotation_matrix[1]-j*rotation_matrix[0]), y+r+i*rotation_matrix[0]+j*rotation_matrix[1]));
+			temp->setFillColor(sf::Color::Red);
+			window->draw(*temp);
+			delete temp;
+			temp = nullptr;
+		}
+	}
+	//v_box[0].position = sf::Vector2f(x-vx_left, y-vy_top);
+	//v_box[1].position = sf::Vector2f(x-vx_left, y-vy_top);
+	//v_box[2].position = sf::Vector2f(x-vx_left, y-vy_top);
+	//v_box[3].position = sf::Vector2f(x-vx_left, y-vy_top);
+	//v_box[0] = sf::Vertex(sf::Vector2f(x+r+vx_left, y-vy_top));
+	//v_box[1] = sf::Vertex(sf::Vector2f(x+r+vx_right, y-vy_top));
+	//v_box[3] = sf::Vertex(sf::Vector2f(x+r+vx_left, y+r));
+	//v_box[2] = sf::Vertex(sf::Vector2f(x+r+vx_right, y+r));
+	//std::cout<<x-vx_left<<std::endl;
+	//window->draw(v_box, 4, sf::Quads);
+
 }
 
 void Agent::update()
 {
 	x++;
+	rotation+=0.1;
+	rotation_matrix[0] = sin(rotation);
+	rotation_matrix[1] = cos(rotation);
+	//vx_left = -(vision_vector[0]+r)*sin(vision_vector[1]+rotation);
+	//vx_right = (vision_vector[0]+r)*sin(vision_vector[1]+rotation);
+	//vy_top = r+vision_vector[0]*cos(rotation);
 	shape.setPosition(x, y);
 }
 
--- Agent.hpp
+++ Agent.hpp
@@ -18,13 +18,31 @@ class Agent
 		
 
 	private:
+
+		//x, y and radius. Note that x and y are measured from the top left corner of the shape
 		int x, y, r;
-		double* (*gene_func_list)(double, double, double);
+
+		double gene_func(int input);
 		sf::CircleShape shape;
+
+		//current rotation of agent measured from y axis
 		double rotation;
+
+		//the type of agent
 		t_agent type;
-		double v_vector[2];
-		struct point<int> constraints[4];
+
+		//vision vector
+		// [0] = radius
+		// [1] = radians left and right from rotation = 0
+		double vision_vector[2];
+
+		//struct point<int> constraints[4];
+		int vx_left, vx_right, vy_top, vy_bottom;
+		
+		//test code
+		double rotation_matrix[2] = {0};
+		//sf::VertexArray v_box(sf::Lines, 4);
+		//sf::Vertex v_box[4];
 
 		//not yet used
 		int energy;
